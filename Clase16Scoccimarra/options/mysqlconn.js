5c54536b154a6afba949f2b2e181d180cc51e593
unknown <fascoccimarra@gmail.com>
2022-12-14 23:23:46 -0300

    Desafio clase 16

---
+++ Clase16Scoccimarra/options/mysqlconn.js
@@ -0,0 +1,13 @@
+const options = {
+    client: 'mysql',
+    connection: {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'mibase'
+    }
+}
+
+export default {
+    options
+}
